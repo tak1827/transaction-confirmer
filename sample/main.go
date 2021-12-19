56967698f0b39e66b357e48f1e18bb36e05c0245
tak <re795h@gmail.com>
2021-12-20 00:26:04 +0900

    refactor sample

--- sample/main.go
+++ sample/main.go
@@ -14,55 +14,9 @@ import (
 )
 
 var (
-	// geth
 	Endpoint = "http://127.0.0.1:8545"
 	PrivKey  = "d1c71e71b06e248c8dbe94d49ef6d6b0d64f5d71b1e33a0f39e14dadb070304a"
-	PrivKey2 = "8179ce3d00ac1d1d1d38e4f038de00ccd0e0375517164ac5448e3acc847acb34"
-	PrivKey3 = "df38daebd09f56398cc8fd699b72f5ea6e416878312e1692476950f427928e7d"
 )
-
-func errHandler(err error) {
-	if err != nil {
-		panic(err.Error())
-	}
-}
-
-func txStore() *store.PrefixStore {
-	leveldb, err := store.NewLevelDB("")
-	errHandler(err)
-	return store.NewPrefixStore(leveldb, pb.PREFIX_PENDING_TX)
-}
-
-func buildTx(client *Client, w *Wallet, txStore *store.PrefixStore) *types.Transaction {
-	var (
-		priv   = w.priv
-		nonce  = w.nonce.Increment()
-		amount = ToWei(1.0, 9) // 1gwai
-	)
-
-	to, err := GenerateAddr()
-	errHandler(err)
-
-	tx, err := client.BuildTx(priv, nonce, to, amount)
-	errHandler(err)
-
-	now := time.Now()
-	transactoin := pb.Transaction{
-		Id:        tx.Hash().Hex(),
-		To:        to.Hex(),
-		Nonce:     tx.Nonce(),
-		Status:    pb.Transaction_PENDING,
-		UpdatedAt: &now,
-	}
-	key := transactoin.StoreKey()
-	value, err := transactoin.Marshal()
-	errHandler(err)
-
-	err = txStore.Put(key, value)
-	errHandler(err)
-
-	return tx
-}
 
 func main() {
 	var (
@@ -114,4 +68,47 @@ func main() {
 		default:
 		}
 	}
+}
+
+func errHandler(err error) {
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func txStore() *store.PrefixStore {
+	leveldb, err := store.NewLevelDB("")
+	errHandler(err)
+	return store.NewPrefixStore(leveldb, pb.PREFIX_PENDING_TX)
+}
+
+func buildTx(client *Client, w *Wallet, txStore *store.PrefixStore) *types.Transaction {
+	var (
+		priv   = w.priv
+		nonce  = w.nonce.Increment()
+		amount = ToWei(1.0, 9) // 1gwai
+	)
+
+	to, err := GenerateAddr()
+	errHandler(err)
+
+	tx, err := client.BuildTx(priv, nonce, to, amount)
+	errHandler(err)
+
+	now := time.Now()
+	transactoin := pb.Transaction{
+		Id:        tx.Hash().Hex(),
+		To:        to.Hex(),
+		Nonce:     tx.Nonce(),
+		Status:    pb.Transaction_PENDING,
+		UpdatedAt: &now,
+	}
+	key := transactoin.StoreKey()
+	value, err := transactoin.Marshal()
+	errHandler(err)
+
+	err = txStore.Put(key, value)
+	errHandler(err)
+
+	return tx
 }
